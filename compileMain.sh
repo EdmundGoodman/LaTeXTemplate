ab4c7513a69e74f0498993675135d494ef18169a
EdmundGoodman <37504168+EdmundGoodman@users.noreply.github.com>
2021-03-16 12:19:15 +0000

    Delete compileMain.sh

--- compileMain.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pdflatex main.tex
-
-#If the "minted" package is being used, the
-#the shell-escape argument is needed to interface
-#properly with the "pygments" backend package
-#pdflatex -shell-escape main.tex
